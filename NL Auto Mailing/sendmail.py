23d5544dc6c4784a31447703c03a404527487957
rolansy <ronalshoeygeorge@gmail.com>
2024-03-10 14:33:53 +0530

    Update email template with HTML content

--- NL Auto Mailing/sendmail.py
+++ NL Auto Mailing/sendmail.py
@@ -1,3 +1,56 @@
+htmlnx = """\
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+    <title>Email Title</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="x-apple-disable-message-reformatting" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+</head>
+
+<body style="background-color:silver;">
+<table style="width:100%;border-spacing:0px;" >
+ 
+<tr> 
+	<td style="width:10%;">&nbsp;</td> 
+	<td style="color: white;background-color:black"><b>&nbsp;Hello {name}, </b> </td>  
+	<td style="width:10%">&nbsp;</td> 
+</tr>
+
+
+<tr> 
+	<td style="width:10%">&nbsp;</td> 
+	<td style="width:80%;background-color:black" > <a href ="https://netxmace.github.io/"><img style="display:block;" width="100%" height="100%" src="cid:image1" alt="newsletter" id="a"></a> </td> 
+	<td style="width:10%">&nbsp;</td> 
+</tr>
+
+<tr> 
+   <td style="width:10%;">&nbsp;</td> 
+   <td style="color: white;font-size:10px;text-align: center;background-color:black"> <i> Mail is send from NetX club MACE</i> </td> 
+   <td style="width:10%">&nbsp;</td> 
+</tr>
+
+<tr> 
+   <td style="width:10%;">&nbsp;</td> 
+   <td style="color: white;font-size:10px;text-align: center;background-color:black"> <i>*Click the image to visit our webpage</i> </td> 
+   <td style="width:10%">&nbsp;</td> 
+</tr>
+ 
+</table>
+
+<body>
+
+</html>
+			
+"""
+
 import csv,smtplib,ssl,urllib.request
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -18,7 +71,7 @@ msgroot.preamble='This is a multi-part message in MIME format.'
 msgalt=MIMEMultipart('alternative')
 msgroot.attach(msgalt)
 
-msghtml=MIMEText('<img src="cid:image1">', 'html')
+msghtml=MIMEText('htmlnx', 'html')
 msgalt.attach(msghtml)
 
 fp=open(newsimg, 'rb')
